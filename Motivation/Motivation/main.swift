045fb1de2fe354c8788afc514b79c47005da4f8e
Aleksei Babenko <elbrujomuerte@gmail.com>
2024-08-03 13:57:34 +0300

    add xcode.files

---
+++ Motivation/Motivation/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Motivation
+//
+//  Created by bruj0 on 3.08.24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let hello = "Hello, mr. Dimchik"
+print(hello)
